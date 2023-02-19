a1ba3d3f9c6cb427343db57f8e6fb5ed6d32b398
CraftyUnknown <100669486+CraftyUnknown@users.noreply.github.com>
2023-02-19 18:07:26 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 -- Scrax ~ by Voidium
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/CraftyUnknown/ssdfDFGjhFKHJkjlhLKHJhkjJH/main/Scrax"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CraftyUnknown/voidium/main/Scrax"))()
 
 -- Games:
 
