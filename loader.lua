ce4d73928105b04c640d75456dec835305024fba
Crafty <100669486+CraftyUnknown@users.noreply.github.com>
2025-02-03 19:05:07 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,10 +1,1 @@
--- Scrax ~ by Voidium
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/CraftyUnknown/voidium/main/lua.lua"))()
-
--- Games:
-
--- Prsion Life
--- Ninja Legends
--- Cart ride into Rdite
--- Race Clicker
+-- SCRAX HAS BEEN DISCONTINUED!!
